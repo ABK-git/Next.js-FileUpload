b7bf2f442b03c2920d7cbabbcfb5962630209d7a
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-02-01 14:19:15 +0900

    ファイルを保存したらすぐに表示するようにした。

--- pages/sign_up/index.jsx
+++ pages/sign_up/index.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Image from 'next/image'
 
 const SignUp = () => {
   let [file, setFile] = useState(null);
+  const [filePath, setFilePath] = useState(null);
 
   //画像のUP構成
   const config = {
@@ -15,31 +17,38 @@ const SignUp = () => {
     },
   };
 
-  const handleClick = async (event) => {
+  const handleClick = async () => {
     const formData = new FormData();
     formData.append("file", file);
 
-    let data = null;
-
-    await axios.post("/api/upload_user_image", formData, config)
-    .then(response => data = response.data.file_name);
-    
-    console.log(data);
+    await axios.post("/api/upload_user_image", formData, config).then((response) => {
+      const path = response.data.file_name.replaceAll("\\","/").slice(6);
+      setFilePath(path);
+    });
   };
 
-  const handleChange = (event) => 
-  {
+  const handleChange = (event) => {
     let file = event.target.files[0];
     console.log(file);
-    
+
     setFile(event.target.files[0]);
   };
 
   return (
-    <form>
-      <input accept="image/*" type="file" name="file" onChange={handleChange} />
-      <button type="button" onClick={handleClick}>送信</button>
-    </form>
+    <div>
+      <form>
+        <input
+          accept="image/*"
+          type="file"
+          name="file"
+          onChange={handleChange}
+        />
+        <button type="button" onClick={handleClick}>
+          送信
+        </button>
+      </form>
+      {filePath ? <Image alt="Sample" src={filePath} width={200} height={200}/> : ""}
+    </div>
   );
 };
 
